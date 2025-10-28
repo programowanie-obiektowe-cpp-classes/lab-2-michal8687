19fff25d2358cf59ec9bd3afd0517ce27f9dd6fa
Michał Tomporowski <michalang400@gmail.com>
2025-10-29 00:40:42 +0100

    zadanie 1

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -5,4 +5,20 @@
 class ResourceManager
 {
     // Twoja implementacja tutaj
+public:
+	ResourceManager()
+	{
+		zasob = new Resource;
+	}
+	~ResourceManager()
+	{
+		delete [] zasob;
+	}
+	ResourceManager(const ResourceManager& t) : zasob{ t.zasob } {}
+	double get()
+	{
+		return zasob[0].get();
+	}
+private:
+	Resource* zasob;
 };
